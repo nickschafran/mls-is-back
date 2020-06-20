6dbc585d8a0f46b143febd8feabb51925e08c99b
nickschafran <nickschafran@gmail.com>
2020-06-20 00:37:52 -0400

    pylint

--- mls_is_back.py
+++ mls_is_back.py
@@ -5,15 +5,15 @@ import pandas as pd
 
 def get_groups(filename):
     """Create lookup where each key is a team and each value is a group."""
-    with open('groups.csv') as f:
+    with open(filename) as f:
         reader = csv.DictReader(f)
         return {row[k]: k for row in reader for k in row if row[k] != ''}
 
 
-def transform_spi(groups, spi):
+def transform_spi(groups, spi_filename):
     """Filter SPI data to MLS & suppplement with group data."""
-    df = pd.read_csv(spi)
-    mls = df.loc[df['league'] == 'Major League Soccer']
+    spi = pd.read_csv(spi_filename)
+    mls = spi.loc[spi['league'] == 'Major League Soccer']
     mls['group'] = mls['name'].map(lambda x: groups[x.upper()])
     averages = mls.groupby(by=['group']).mean()
     mls = mls.merge(averages, on='group', suffixes=('', '_group_avg'))
@@ -24,8 +24,12 @@ def transform_spi(groups, spi):
 def predict_mls_is_back(mls):
     """Pick top two from each group and add next four best teams."""
     groups = mls.groupby(['group'])
-    top_twelve = groups.apply(lambda x: x.nlargest(2, columns='diff'))
-    next_four = groups.apply(lambda x: x.nlargest(3, columns='diff')[-1:]).nlargest(4, ['diff'])
+    top_twelve = groups.apply(
+        lambda x: x.nlargest(2, columns='diff')
+    )
+    next_four = groups.apply(
+        lambda x: x.nlargest(3, columns='diff')[-1:]
+    ).nlargest(4, ['diff'])
     return pd.concat([top_twelve, next_four])
 
 
